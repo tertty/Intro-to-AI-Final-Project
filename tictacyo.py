2d539d9ddf75dd3358c6493ed15745c94c0298c4
tertty <taylor@stanivision.com>
2020-04-16 14:40:52 -0400

    The distance, I went there

--- tictacyo.py
+++ tictacyo.py
@@ -10,64 +10,143 @@ class graph_map:
     #What makes up a node, includes possible moves. 
     def __init__(self, board_size=None, board=None): # Constructor  
         self.board_size = board_size
-        self.board = ["-"] * (board_size * board_size)
+        self.board = ['-'] * (board_size * board_size)
         self.total_moves = (board_size * board_size)
         self.total_available_moves = (board_size * board_size)
         self.depth_check = False
-        
-    #Checks to see in the Computer moves equal to a win (3 in a row)
-    def computer_win(self):
+
+    def show_hint(self):
         new_line = 1
-        diag_list = []
         for i in range(self.total_moves):
             if(new_line == self.board_size):
-                diag_list.append(self.board[i])
-
-                ex = 0
-                dash = 0
-
-                for a in diag_list:
-                    if(a == 'X'):
-                        ex += 1
-                    if(a == '-'):
-                        dash += 1
-
-                if((ex == self.board_size-1) and (dash == 1)):
-                    return True
-                else:
-                    new_line = 1
-                    diag_list = []
+                print(i)
+                new_line = 1
             else:
-                diag_list.append(self.board[i])
+                print(i, end='')
                 new_line += 1
+        
+    def check_for_win(self):
+        if(self.computer_win()):
+            print("Computer wins, game over!")
+            return True
+        elif(self.human_win()):
+            print("Human wins, game over!")
+            return True
         return False
+
+    #Checks to see in the Computer moves equal to a win (3 in a row)
+    def computer_win(self):
+
+        #Vertical checks
+        vertical_answer = False
+
+        for k in range(0,self.total_moves,self.board_size):
+            x_check = 0
+            for j in range(self.board_size):
+                if(self.board[k+j] == 'X'):
+                    x_check += 1
+            if(x_check == self.board_size):
+                vertical_answer = True
+            x_check = 0
+
+        #Horizontal checks
+        horizontal_answer = False
+
+        for i in range(self.board_size):
+            x_check = 0
+            for j in range(0,self.total_moves,self.board_size):
+                if(self.board[i+j] == 'X'):
+                    x_check += 1
+            if(x_check == self.board_size):
+                horizontal_answer = True
+            x_check = 0
+
+        #Diagnal right checks
+        diagnal_right_answer = False
+
+        x_check = 0
+
+        for i in range(0,self.total_moves, self.board_size+1):
+            if(self.board[i] == 'X'):
+                x_check += 1
+
+        if(self.board[self.total_moves-1] == 'X'):
+            x_check += 1
+
+        if(x_check == self.board_size):
+            diagnal_right_answer == True
+
+        #Diagnal left checks
+        diagnal_left_answer = False
+
+        x_check = 0
+
+        for i in range(self.board_size-1,self.total_moves-(self.board_size-1), self.board_size-1):
+            if(self.board[i] == 'X'):
+                x_check += 1
+
+        if(x_check == self.board_size):
+            diagnal_left_answer == True
+
+        return (vertical_answer or horizontal_answer or diagnal_right_answer or diagnal_left_answer)
         
     #Checks to see in the Human moves equal to a win (3 in a row)
     def human_win(self):
-        new_line = 1
-        diag_list = []
-        for i in range(self.total_moves):
-            if(new_line == self.board_size):
-                diag_list.append(self.board[i])
 
-                oh = 0
-                dash = 0
+        #Vertical checks
+        vertical_answer = False
 
-                for a in diag_list:
-                    if(a == 'O'):
-                        oh += 1
-                    if(a == '-'):
-                        dash += 1
+        for k in range(0,self.total_moves,self.board_size):
+            o_check = 0
+            for j in range(self.board_size):
+                if(self.board[k+j] == 'O'):
+                    o_check += 1
+            if(o_check == self.board_size):
+                vertical_answer = True
+            o_check = 0
 
-                if((oh == self.board_size-1) and (dash == 1)):
-                    return True
-                else:
-                    new_line = 1
-                    diag_list = []
-            else:
-                diag_list.append(self.board[i])
-                new_line += 1
-        return False
+        #Horizontal checks
+        horizontal_answer = False
+
+        for i in range(self.board_size):
+            o_check = 0
+            for j in range(0,self.total_moves,self.board_size):
+                if(self.board[i+j] == 'O'):
+                    o_check += 1
+            if(o_check == self.board_size):
+                horizontal_answer = True
+            o_check = 0
+
+        #Diagnal right checks
+        diagnal_right_answer = False
+
+        o_check = 0
+
+        for i in range(0,self.total_moves, self.board_size+1):
+            if(self.board[i] == 'O'):
+                o_check += 1
+
+        if(self.board[self.total_moves-1] == 'O'):
+            o_check += 1
+
+        if(o_check == self.board_size):
+            diagnal_right_answer == True
+
+        #Diagnal left checks
+        diagnal_left_answer = False
+
+        o_check = 0
+
+        for i in range(self.board_size-1,self.total_moves-(self.board_size-1), self.board_size-1):
+            if(self.board[i] == 'O'):
+                o_check += 1
+
+        if(o_check == self.board_size):
+            diagnal_left_answer = True
+        
+        #print(o_check)
+
+        return (vertical_answer or horizontal_answer or diagnal_right_answer or diagnal_left_answer)
 
     #Checks to see if the board is full and if it is then end the game
     def full_board(self):
@@ -92,14 +171,14 @@ class graph_map:
         new_move = 0
 
         if((self.depth_check == True) and (depth > 3)):
-            return 1
+            return -1
 
         if(self.full_board()):
             #print("COMPUTER DRAW!")
             return 0
         elif(self.computer_win()):
             #print("Human wins!")
-            #self.print_game_board() Debug
+            #self.print_game_board()
             return -1
         else:
             value = float('-inf')
@@ -120,13 +199,14 @@ class graph_map:
         new_move = 0
 
         if((self.depth_check == True) and (depth > 3)):
-            return -1
+            return 1
 
         if(self.full_board()):
             #print("HUMAN DRAW!")
             return 0
         elif(self.human_win()):
             #print("Computer wins!")
+            #self.print_game_board()
             return 1
         else:
             value = float('inf')
@@ -155,12 +235,14 @@ def tictactoe_homework():
     game_board.board[game_board.computer_move(0)] = 'X'
     game_board.total_available_moves -= 1
 
+    print("Current board:")
+    game_board.print_game_board()
+
     while(game_board.total_available_moves > 0):
         print("Moves left:", game_board.total_available_moves)
 
-        game_board.print_game_board()
-
         if (N_ARGS.M == "H"):
+            game_board.show_hint()
             show_me_your_moves = input("Your move, human>>")
             game_board.board[int(show_me_your_moves)] = 'O'
         else:    
@@ -169,15 +251,31 @@ def tictactoe_homework():
             print("Human move:", human_move)
             game_board.board[human_move] = 'O'
         game_board.total_available_moves -= 1
+        if(game_board.check_for_win()):
+            print("Final board:")
+            game_board.print_game_board()
+            return
+
+        print("Current board:")
+        game_board.print_game_board() 
 
         #Computer automated move
         computer_move = game_board.computer_move(0)
         print("Computer move:", computer_move)
         game_board.board[computer_move] = 'X'
         game_board.total_available_moves -= 1
+        if(game_board.check_for_win()):
+            print("Final board:")
+            game_board.print_game_board()
+            return 
 
+        print("Current board:")
+        game_board.print_game_board() 
+
+    print("Final board:")
     game_board.print_game_board()
-    print("Game over!")
+    if(not game_board.check_for_win()): 
+        print("Draw, game over!")
 
 #This calls the 'main'
 if __name__ == "__main__":
